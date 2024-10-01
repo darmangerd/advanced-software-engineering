33db2d88e00cfe110d2ae00a806b679658829c15
David Darmanger <david.darmanger@protonmail.com>
2024-10-01 10:46:54 +0200

    Minor fix

--- init_db.sql
+++ init_db.sql
@@ -19,15 +19,15 @@ CREATE TABLE todos_tags (
     FOREIGN KEY (tag_ref_id) REFERENCES tags (id) ON DELETE CASCADE
 );
 
-INSERT INTO tags (title) VALUES ('tag1');
-INSERT INTO tags (title) VALUES ('tag2');
-INSERT INTO tags (title) VALUES ('tag3');
+-- INSERT INTO tags (title) VALUES ('tag1');
+-- INSERT INTO tags (title) VALUES ('tag2');
+-- INSERT INTO tags (title) VALUES ('tag3');
 
-INSERT INTO todos (title, completed, `order`) VALUES ('todo1', false, 1);
-INSERT INTO todos (title, completed, `order`) VALUES ('todo2', true, 2);
-INSERT INTO todos (title, completed, `order`) VALUES ('todo3', false, 3);
+-- INSERT INTO todos (title, completed, `order`) VALUES ('todo1', false, 1);
+-- INSERT INTO todos (title, completed, `order`) VALUES ('todo2', true, 2);
+-- INSERT INTO todos (title, completed, `order`) VALUES ('todo3', false, 3);
 
-INSERT INTO todos_tags (tag_ref_id, todo_ref_id) VALUES (1, 1);  
-INSERT INTO todos_tags (tag_ref_id, todo_ref_id) VALUES (2, 1);  
-INSERT INTO todos_tags (tag_ref_id, todo_ref_id) VALUES (2, 2);  
-INSERT INTO todos_tags (tag_ref_id, todo_ref_id) VALUES (3, 3); 
+-- INSERT INTO todos_tags (tag_ref_id, todo_ref_id) VALUES (1, 1);  
+-- INSERT INTO todos_tags (tag_ref_id, todo_ref_id) VALUES (2, 1);  
+-- INSERT INTO todos_tags (tag_ref_id, todo_ref_id) VALUES (2, 2);  
+-- INSERT INTO todos_tags (tag_ref_id, todo_ref_id) VALUES (3, 3); 
